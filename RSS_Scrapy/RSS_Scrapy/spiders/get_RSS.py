aa5886bd2ca9e9bb27c922129b3f770ec2456154
Etaac <s406411156@gms.tku.edu.tw>
2019-09-10 17:04:31 +0800

    create test.html

--- RSS_Scrapy/RSS_Scrapy/spiders/get_RSS.py
+++ RSS_Scrapy/RSS_Scrapy/spiders/get_RSS.py
@@ -4,5 +4,23 @@ from RSS_Scrapy.RSS_Scrapy.items import RssReader
 
 class RSSSpider(scrapy.Spider):
     name = 'RSS'
-    allowed_domains = ['']
+    start_urls = [
+        'http://feeds.feedburner.com/yuminghui',
+        'http://feeds.feedburner.com/CommaTravel',
+        'https://www.weekendhk.com/feed/',
+        'https://itravelblog.net/feed/',
+        'https://viablog.okmall.tw/blog/rss.php'
+    ]
+    custom_settings = {
+        'FEED_EXPORT_ENCODING': 'utf-8',
+        'DOWNLOAD_DELAY': 2,
+        'RANDOMIZE_DOWNLOAD_DELAY': True
+    }
+
+    def parse(self, response):
+
+
+
+
+
 
